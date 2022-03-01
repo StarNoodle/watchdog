dd1c685d41c2c1ad0f8ab4e84a242ec02de1760d
Aymeric <georgin.aymeric@gmail.com>
2022-03-01 15:07:24 +0100

    <Feature> format incoming data for directed force graph

--- src/app.js
+++ src/app.js
@@ -25,12 +25,15 @@ const main = () => {
 
               $("#content #owner_name").append("<br/> <br/>");
 
+              // formatForceGraphData(list);
               const element = document.createElement("div");
               for (let mediaRelationships of list) {
                 const relationsElement = document.createElement("p");
                 for (let relation of mediaRelationships) {
                   $(relationsElement).append(
-                    `<div>${relation.name} ${relation.valeur || ""}</div>`
+                    `<div>${relation.name}</div><div>${relation.valeur || ""}${
+                      relation.valeur ? "%" : ""
+                    }</div>`
                   );
                 }
                 $(element).append(relationsElement);
@@ -44,9 +47,95 @@ const main = () => {
         );
       } else {
         $("#content #content_test").text("Une erreur est survenue");
-        // throw new Error("No url detected");
       }
     });
 };
 
 main();
+
+/*
+@params data = [
+  [
+    { name: "Xavier Niel", value: 50 },
+    { name: "Fonds idepedant de la Presse", value: 50 },
+    { name: "Le Monde Libre", value: 50 },
+    { name: "Le Monde SA", value: 50 },
+    { name: "Le Monde" },
+  ],
+  [
+    { name: "Mathieu Pigasse", value: 50 },
+    { name: "Combat Solution", value: 50 },
+    { name: "Le Nouveau Monde", value: 50},
+    { name: "Le Monde Libre", value: 50 },
+    { name: "Le Monde SA", value: 50 },
+    { name: "Le Monde" },
+  ],
+  [
+    { name: "Daniel Kretinsky", value: 50 },
+    { name: "Czech Media Invest", value: 50 },
+    { name: "Le Nouveau Monde", value: 50},
+    { name: "Le Monde Libre", value: 50 },
+    { name: "Le Monde SA", value: 50 },
+    { name: "Le Monde" },
+  ]
+]
+
+@return  nodes = [
+    { name: "Le Monde", type: "start" },
+    { name: "Le Monde SA", target: 0, value: 50, type: "middle" },
+    { name: "Le Monde Libre", target: 1, value: 50, type: "middle" },
+    { name: "Le Nouveau Monde", target: 2, value: 50, type: "middle" },
+    { name: "Fonds idepedant de la Presse", target: 2, value: 50, type: "middle" },
+    { name: "Combat Solution", target: 3, value: 50, type: "middle" },
+    { name: "Czech Media Invest", target: 3, value: 50, type: "middle" },
+    { name: "Xavier Niel", target: 4, value: 50, type: "end" },
+    { name: "Mathieu Pigasse", target: 5, value: 50, type: "end" },
+    { name: "Daniel Kretinsky", target: 6, value: 50, type: "end" },
+  ];
+*/
+function formatForceGraphData(data) {
+  // each first element are type start and last are end, other are middle
+  const reverseDataWithType = attributeType(data.map((_d) => _d.reverse()));
+
+  const result = [];
+  // push uniq element in array with target
+  reverseDataWithType.forEach((_rdwt) => {
+    _rdwt.forEach((_rd, _i) => {
+      const index = result.findIndex(
+        (_r) => _r.name === _rd.name && _r.valeur === _rd.valeur
+      );
+      if (index === -1) {
+        if (_i > 0) {
+          const ancestor = _rdwt[_i - 1];
+          const targetAncestorIndex = result.findIndex(
+            (_ta) =>
+              _ta.name === ancestor.name && _ta.valeur === ancestor.valeur
+          );
+          result.push(Object.assign({}, _rd, { target: targetAncestorIndex }));
+        } else {
+          result.push(Object.assign({}, _rd));
+        }
+      }
+    });
+  });
+
+  return result;
+}
+
+const attributeType = (data) => {
+  let cloneData = Array.from(data);
+
+  for (let i = 0; i < cloneData.length; i++) {
+    for (let j = 0; j < cloneData[i].length; j++) {
+      if (j === 0) {
+        cloneData[i][j].type = "start";
+      } else if (j === cloneData[i].length - 1) {
+        cloneData[i][j].type = "end";
+      } else {
+        cloneData[i][j].type = "middle";
+      }
+    }
+  }
+
+  return cloneData;
+};
